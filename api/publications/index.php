58d95c90659ef520f49a48c80f486743a98c2922
Denis Sokolov <denis.sokolov@leroymerlin.ru>
2024-01-25 00:07:22 +0300

    add task 007

---
+++ api/publications/index.php
@@ -0,0 +1,35 @@
+<?php
+
+define('ROOT', dirname(__FILE__));
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+    $request = json_decode(file_get_contents("php://input"));
+    $json = [];
+
+    if (file_exists(ROOT . '/publications.json')) {
+        $json = json_decode(file_get_contents(ROOT . '/publications.json'));
+    }
+    
+    $json[] = stripslashes(substr($request->publication, 0, 256));
+
+    file_put_contents(ROOT . '/publications.json', json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+
+    header('HTTP/1.1 201 Created');
+}
+
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'get') {
+    $response = [];
+
+    if (file_exists(ROOT . '/publications.json')) {
+        $response = json_decode(file_get_contents(ROOT . '/publications.json'));
+    }
+
+    header('HTTP/1.1 200 OK');
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+
+echo json_encode($response);
+
+?>
